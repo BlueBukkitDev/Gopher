ce29a9caeef42a20868355286f3ba7bf74131ba9
Jonathan Bruce <bluebukkitdev@gmail.com>
2022-07-07 15:52:33 -0400

    Added the Order Reports page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/about'
+  get 'home/order_reports'
 
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
