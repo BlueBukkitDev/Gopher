db44b10e74740b9ac2e2375f9f9c24e934a850f1
Lemon <kcwe891@gmail.com>
2022-07-08 10:37:50 -0400

    Updated Menu Options
    Menu options updated. About Us page removed. Slight CSS adjustments.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/about'
+  get 'home/allocated'
   get 'home/order_reports'
+  get 'home/shipping_reports'
 
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
