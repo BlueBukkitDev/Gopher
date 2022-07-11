6d7881a8bbc41f663b86d9ccf0a3501544b530cd
Jonathan Bruce <bluebukkitdev@gmail.com>
2022-07-11 10:27:26 -0400

    Tried

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,7 +20,9 @@ class Item < ApplicationRecord
 
 	def get_toOrder
 		if !self.reorder == nil && !self.inshop == nil
-			self.reorder-self.inshop #2 - 5 //reorder at 2, we have 5, we need to order -3
+			if self.reorder-self.inshop < 0#2 - 5 //reorder at 2, we have 5, we need to order -3
+				0
+			else self.reorder-self.inshop
 		else 0
 		end
 	end
