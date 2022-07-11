d8f2f5f2aedada709d2f062c0a1d6e7e15744b47
Jonathan Bruce <bluebukkitdev@gmail.com>
2022-07-11 10:24:04 -0400

    Styling and fat updates to the table and asst ruby

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,12 +5,24 @@ class Item < ApplicationRecord
 	end
 
 	def get_inStock
-		self.inshop
+		if !self.inshop == nil
+			self.inshop
+		else 0
+		end
 	end
 
 	def get_partNumber
-		self.partnumber
+		if !self.partnumber == nil
+			self.partnumber
+		else "#______"
+		end
 	end
 
+	def get_toOrder
+		if !self.reorder == nil && !self.inshop == nil
+			self.reorder-self.inshop #2 - 5 //reorder at 2, we have 5, we need to order -3
+		else 0
+		end
+	end
 end
 #<Item id: 1, itemname: "Item 4", partnumber: nil, description: "Some generic description to define what this item ...", inshop: nil, allocated: nil, ordered: nil, reorder: nil, stockcap: nil, category: nil, verifiedwhen: nil, verifiedby: nil, unit: nil, supplier0: nil, supplier1: nil, supplier2: nil, supplier3: nil, supplier4: nil, supplier5: nil, supplier6: nil, supplier7: nil, supplier8: nil, supplier9: nil, created_at: "2022-07-08 19:35:20.771164000 +0000", updated_at: "2022-07-08 19:35:20.771164000 +0000">
